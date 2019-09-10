1589dbf7baf276d60c5a710fa74c1c0bae6dade9
Iddan Aaronsohn <mail@aniddan.com>
2019-09-10 19:17:45 +0300

    Save queries

--- src/App.tsx
+++ src/App.tsx
@@ -7,28 +7,62 @@ import MonacoEditor from "@monaco-editor/react";
 import { useEditor } from "./monaco-util";
 import "./App.css";
 
-function App() {
-  const [handleEditorMount, editor] = useEditor();
-  const [result, setResult] = useState("");
+type Query = {
+  id: number;
+  text: string;
+  result: string | null;
+  time: Date;
+};
 
-  const handleClick = React.useCallback(
-    event => {
-      console.log(editor.getValue());
-      fetch("http://localhost:64210/api/v1/query/gizmo", {
-        method: "POST",
-        body: editor.getValue()
+const SERVER_URL = "http://localhost:64210";
+const ACTIVE_QUERY_INITIAL_STATE: number | null = null;
+const QUERIES_INITIAL_STATE: Query[] = [];
+
+function App() {
+  const [language, setLanguage] = useState("gizmo");
+  const [activeQuery, setActiveQuery] = useState(ACTIVE_QUERY_INITIAL_STATE);
+  const [queries, setQueries] = useState(QUERIES_INITIAL_STATE);
+  const [handleEditorMount, editor] = useEditor();
+
+  const handleClick = React.useCallback(() => {
+    const id = queries.length;
+    const query = editor.getValue();
+    setActiveQuery(id);
+    setQueries(queries => [
+      ...queries,
+      { id, text: query, result: null, time: new Date() }
+    ]);
+    fetch(`${SERVER_URL}/api/v1/query/${language}`, {
+      method: "POST",
+      body: query
+    })
+      .then(res => res.text())
+      .then(result => {
+        setQueries(queries =>
+          queries.map(query => {
+            if (query.id === id) {
+              return { ...query, result };
+            } else {
+              return query;
+            }
+          })
+        );
       })
-        .then(res => res.text())
-        .then(res => setResult(res));
-    },
-    [editor]
-  );
+      .catch(error => {
+        alert(error);
+      });
+  }, [editor, queries]);
+
+  const currentQuery = queries.find(query => query.id === activeQuery);
+  const result = currentQuery && currentQuery.result;
 
   let formattedResult = result;
 
-  try {
-    formattedResult = JSON.stringify(JSON.parse(result), null, 4);
-  } catch {}
+  if (result) {
+    try {
+      formattedResult = JSON.stringify(JSON.parse(result), null, 4);
+    } catch {}
+  }
 
   return (
     <div className="App">
@@ -41,6 +75,15 @@ function App() {
             language="javascript"
           />
           <Button label="Run" onClick={handleClick} />
+        </Card>
+        <div>Results | Query History</div>
+        <Card>
+          {[...queries].reverse().map(query => (
+            <div>
+              {query.time.toLocaleString()} result?{" "}
+              {query.result ? "yes" : "no"}
+            </div>
+          ))}
         </Card>
         <Card className="Results">
           <MonacoEditor
