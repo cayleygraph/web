9c2f91480dd5200cba885b6c3918a7ef794debf2
Iddan Aaronsohn <mail@aniddan.com>
2019-09-21 22:19:27 +0300

    Add QueryEditor to VisualizePage

--- src/VisualizePage.tsx
+++ src/VisualizePage.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import QueryEditor from "./QueryEditor";
 
 type Props = { serverURL: string };
 
 const VisualizePage = ({ serverURL }: Props) => {
-  return <>Visualize!</>;
+  return (
+    <main>
+      <QueryEditor onRun={(...args) => console.log("Visualize", ...args)} />
+    </main>
+  );
 };
 
 export default VisualizePage;
