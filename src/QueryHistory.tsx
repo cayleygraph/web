f89594c101b5ece0eea11134e87f1e5df281688c
Iddan Aaronsohn <mail@aniddan.com>
2019-09-21 21:35:39 +0300

    Show lang in history

--- src/QueryHistory.tsx
+++ src/QueryHistory.tsx
@@ -3,27 +3,32 @@ import { List, ListItem } from "@rmwc/list";
 import "@material/list/dist/mdc.list.css";
 import { Icon } from "@rmwc/icon";
 import "@rmwc/icon/icon.css";
-import { Query } from "./queries";
+import { Query, languageOptions } from "./queries";
 import "./QueryHistory.css";
 
 const QueryHistory = ({ queries }: { queries: Query[] }) => (
   <List className="QueryHistory">
-    {[...queries].reverse().map(query => (
-      <ListItem>
-        <div className="time">{query.time.toLocaleString()}</div>
-        <div className="time">{query.language}</div>
-        <div className="status">
-          {query.result ? (
-            "error" in query.result ? (
-              <Icon icon="error" />
-            ) : (
-              <Icon icon="check_circle" />
-            )
-          ) : null}
-        </div>
-        <div className="query">{query.text}</div>
-      </ListItem>
-    ))}
+    {[...queries].reverse().map(query => {
+      const option = languageOptions.find(
+        option => option.value === query.language
+      );
+      return (
+        <ListItem>
+          <div className="time">{query.time.toLocaleString()}</div>
+          <div className="status">
+            {query.result ? (
+              "error" in query.result ? (
+                <Icon icon="error" />
+              ) : (
+                <Icon icon="check_circle" />
+              )
+            ) : null}
+          </div>
+          <div className="language">{option && option.label}</div>
+          <div className="query">{query.text}</div>
+        </ListItem>
+      );
+    })}
   </List>
 );
 
