0195c9b37159c3e75b5fd566b416c148b0b42705
Iddan Aaronsohn <mail@aniddan.com>
2019-09-29 16:33:41 +0300

    MVP QueryShapePage

--- src/QueryShapePage.tsx
+++ src/QueryShapePage.tsx
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import QueryEditor from "./QueryEditor";
+import JSONCodeViewer from "./JSONCodeViewer";
 
 type Props = { serverURL: string };
 
+async function getShape(serverURL: string, query: string) {
+  const res = await fetch(`${serverURL}/api/v1/shape/gizmo`, {
+    method: "POST",
+    body: query
+  });
+  const { error, ...result } = await res.json();
+  if (error) {
+    throw new Error(error);
+  }
+  return result;
+}
+
 const QueryShapePage = ({ serverURL }: Props) => {
+  const [result, setResult] = useState<object | null>(null);
+  const run = useCallback(
+    query => {
+      getShape(serverURL, query).then(setResult);
+    },
+    [serverURL]
+  );
   return (
     <main>
-      <QueryEditor onRun={(...args) => console.log("Query Shape", ...args)} />
+      <QueryEditor onRun={run} />
+      <JSONCodeViewer value={result} />
     </main>
   );
 };
