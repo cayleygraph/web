8cd9f95c46acaca086acc96af49499aa6cb8cfea
Iddan Aaronsohn <mail@aniddan.com>
2020-02-15 14:32:26 +0200

    Fix not found

--- src/Entities/NotFound.tsx
+++ src/Entities/NotFound.tsx
@@ -3,7 +3,12 @@ import React from "react";
 const NotFound = () => {
   return (
     <div className="NotFound">
-      <h1>🤷 No entity found</h1>
+      <h1>
+        <span role="img" aria-label="not found">
+          🤷
+        </span>{" "}
+        No entity found
+      </h1>
     </div>
   );
 };
