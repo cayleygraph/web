ec6587e7f3cbd9e6217a003db6398acf29ea3081
Iddan Aaronsohn <mail@aniddan.com>
2020-02-21 12:57:52 +0200

    Format search

--- src/Entities/Search.tsx
+++ src/Entities/Search.tsx
@@ -2,6 +2,7 @@ import React, { useCallback } from "react";
 import AsyncSelect from "react-select/async";
 import { Theme } from "react-select";
 import "./Search.css";
+
 import { getAutoCompletionSuggestions, JsonLdReference } from "./data";
 import { PRIMARY, LIST_ITEM_ACTIVE, LIST_ITEM_HOVER } from "./colors";
 import EntityValue from "./EntityValue";
